15780dc5e93ba5b26fa6a822486425c9e78ed84b
NoorAzar11 <nooraaz945@gmail.com>
2021-06-15 01:37:20 +0300

    lab7

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const server = express();
+const weatherData=require('./data/weather.json');
+const cors = require('cors');
+server.use(cors());
+
+const PORT=3012;
+//`dotenv`, and `cors`.
+server.listen(PORT,()=> { //in order to build a server and give it a port
+    console.log(`lisning on ${PORT}`)
+})
+
+//localhost:3012/test
+server.get('/',(req,re)=>{
+    res.send(data);
+ })
+//localhost:3012/test/getweather
+//to get weather data
+server.get('/getweather',(req,res)=>{
+    let weatherDetails=weatherData.data.map(item=>{
+        return item.data;
+    })
+    //we should write it after retrun and map
+    res.send(weatherDetails);
+
+})
+//localhost:3012/test/getweather?weatherdes
+
+server.get('/getweather',(req,res)=>{
+    let weatherParmeter=req.query.weatherDetails; //to get the value of the paramter itself
+    let weatherdes=weatherData.data.find(item =>{
+if (item.lat === weatherParmeter && item.lan === weatherParmeter)
+return item;
+
+    })
+    res.send(weatherdes);
+})
+
+//localhost:3012/test.....
+server.get('*',(req,res)=> { //this will server eveything
+        res.status(404).send('Please Try Again 404 Error')
