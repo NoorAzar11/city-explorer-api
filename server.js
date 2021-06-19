2cb17dfc77021a0dc7c3814067a41e245e53161d
NoorAzar11 <nooraaz945@gmail.com>
2021-06-19 23:26:12 +0300

    updated

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const weatherData = require('./data/weather.json');
 const cors = require('cors');
 const axios = require('axios');
 require('dotenv').config();
+const Movies=require('./Movies');
+const Weather=require('./Weather');
 
 const PORT = process.env.PORT;
 server.use(cors()); //make our sever open to others
@@ -17,73 +19,58 @@ server.get('/', (req, re) => {
 
 })
 
+//app.get weather and get movies in case exporting obj have more than one fun
+server.get('/weather',Weather)
+server.get('/',(req,res) => {
+res.status(200).send('checking')
+}
+
+server.get('/movies',Movies)
 //localhost:3012/test/getweather
 //to get weather data
 //we should write it after retrun and map
-server.get('/getweather', (req, res) => {
-    let weatherDetails = weatherData.data.map(item => {
-        return item.data;
-    })
-    res.send(weatherDetails);
-
-})
-console.log(weatherData);
-
-server.get('/weather', (req, res) => {
-    let arEa = req.query;
-    let daTa2 = [];
-    let ciTy = weatherData.find(
-        (item) => item.city_name.toLowerCase() === arEa.city_name.toLowerCase()
-    );
-    if (ciTy) {
-        class Forecast {
-            constructor(date, description) {
-                this.date = date;
-                this.description = description;
-
-            }
-        }
-        ciTy.data.forEach((item) => {
-            daTa2.push(new Forecast(item.valid_date, item.weather.description));
-            
-        });
-        // console.log(daTa2);
-       
-res.status(200).send(daTa2);
-
-    }else {
-    res
-        .status(500)
-        .send(`Try Again`)
-        console.log(daTa2);
-    }
-});
-//localhost:3012/test/getweather?weatherdes
+//https://api.themoviedb.org/3/movie/550?api_key=48acd361dfcbf9f19833ecbc0996f5ae
+//https://api.weatherbit.io/v2.0/
 
 // server.get('/getweather', (req, res) => {
-//     let weatherParmeter = req.query.weatherDetails; //to get the value of the paramter itself
-//     let weatherdes = weatherData.data.find(item => {
-//         if (item.lon && item.lan)
-//             return item;
-//             console.log(weatherdes);
+//     let weatherDetails = weatherData.data.map(item => {
+//         return item.data;
 //     })
-
-//     res.send(weatherdes);
+//     res.send(weatherDetails);
 
 // })
+// console.log(weatherData);
 
+// server.get('/weather', (req, res) => {
+//     let arEa = req.query;
+//     let daTa2 = [];
+//     let ciTy = weatherData.find(
+//         (item) => item.city_name.toLowerCase() === arEa.city_name.toLowerCase()
+//     );
+//     if (ciTy) {
+//         class Forecast {
+//             constructor(date, description) {
+//                 this.date = date;
+//                 this.description = description;
 
+//             }
+//         }
+//         ciTy.data.forEach((item) => {
+//             daTa2.push(new Forecast(item.valid_date, item.weather.description));
+            
+//         });
+        // console.log(daTa2);
+       
+// res.status(200).send(daTa2);
 
-// class Weather {
-//     constructor(date) {
-//         this.date = date.valid_date;
-// this.description = date.weather.description;
+//     }else {
+//     res
+//         .status(500)
+//         .send(`Try Again`)
+//         console.log(daTa2);
+//     }
+// });
 
-//     }}
-//     console.log(this.date.valid_date);
-
-//localhost:3012/test....
-//this will server eveything
 
 server.get('*', (req, res) => {
     res.status(404).send('Please Try Again 404 Error');
